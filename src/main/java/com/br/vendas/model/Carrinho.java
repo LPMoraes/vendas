38488270327371961ada607647406f001c945172
Luis Moraes <l7moraes@gmail.com>
2019-05-17 15:27:05 -0300

    criando os models Cliente, Carrinho e Produto

--- src/main/java/com/br/vendas/model/Carrinho.java
+++ src/main/java/com/br/vendas/model/Carrinho.java
@@ -1,13 +1,12 @@
 package com.br.vendas.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
+import java.util.List;
 
 
 @Data
@@ -15,13 +14,18 @@ import java.io.Serializable;
 @AllArgsConstructor
 
 @Entity
-@Table(name = "cliente")
-public class Cliente implements Serializable {
+@Table(name = "carrinho")
+public class Carrinho implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty(message = "nome do cliente é campo obrigatório!")
-    private String nome;
+    @ManyToOne
+    @JoinColumn(name = "id_cliente", foreignKey = @ForeignKey(name = "FK_carrinho_cliente"), nullable = false)
+    private Cliente cliente;
+
+    @OneToMany
+    private List<Produto> produtos;
+
 }
--- src/main/java/com/br/vendas/model/Produto.java
+++ src/main/java/com/br/vendas/model/Produto.java
@@ -1,27 +1,31 @@
 package com.br.vendas.model;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import java.io.Serializable;
 
+import javax.validation.constraints.Size;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 
 @Entity
-@Table(name = "cliente")
-public class Cliente implements Serializable {
+@Table(name="produto")
+public class Produto implements Serializable {
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotEmpty(message = "nome do cliente é campo obrigatório!")
+
+    @NotEmpty(message = "protudo é obrigatório!")
+    @Size(min=2, max=30)
     private String nome;
+
 }
